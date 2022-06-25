ad8fa0e2114e1e89d0b8aa1d22daa4b63f7fe8fc
Alliston <alliston@outlook.com>
2022-06-25 16:04:56 -0300

    Initial Commit

---
+++ GameNet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameNet
+//
+//  Created by Alliston Aleixo on 25/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GameNet/GameNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameNetApp.swift
+//  GameNet
+//
+//  Created by Alliston Aleixo on 25/06/22.
+//
+
+import SwiftUI
+
+@main
+struct GameNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
