e790a96735ad2235a73168e59d03a8bf47c6114d
Alliston Carlos Rosa Aleixo <alliston@outlook.com>
2023-01-06 16:27:14 -0300

    [Watch] Feature: create Apple Watch app

---
+++ GameNet.Watch Watch App/GameNet_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameNet_WatchApp.swift
+//  GameNet.Watch Watch App
+//
+//  Created by Alliston Aleixo on 06/01/23.
+//
+
+import SwiftUI
+
+@main
+struct GameNet_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
